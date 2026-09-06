9f94557a9c02b1bb947503e1698d7897258a9985
PanfilovYE <yepanfilov@gmail.com>
2026-09-06 17:41:50 +0300

    Add brain-calc game with common engine

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import greetUser from '../src/cli.js';
-import runEvenGame from '../src/games/even.js';
+import runGame from '../src/index.js';
+import { getQuestionAndAnswer, gameRule } from '../src/games/even.js';
 
-const userName = greetUser();
-runEvenGame(userName);
+runGame(gameRule, getQuestionAndAnswer);
--- src/games/even.js
+++ src/games/even.js
@@ -1,29 +1,14 @@
-import readlineSync from 'readline-sync';
-
 const isEven = (num) => num % 2 === 0;
 
-const runEvenGame = (name) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  const rounds = 3;
-
-  for (let i = 0; i < rounds; i += 1) {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return; // завершаем игру при ошибке
-    }
-
-    console.log('Correct!');
-  }
-
-  console.log(`Congratulations, ${name}!`);
+const getQuestionAndAnswer = () => {
+  const number = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return {
+    question: String(number),
+    correctAnswer,
+  };
 };
 
-export default runEvenGame;
+const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+export { getQuestionAndAnswer, gameRule };
